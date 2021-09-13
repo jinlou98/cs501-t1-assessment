77544178235c9f5718eb74691d5f2476e7737d1a
Jin Lou <jinlou@dhcp-wifi-8021x-155-41-86-6.bu.edu>
2021-09-13 16:12:28 -0400

    Move changes to migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/venv bash
+#!/bin/bash
 
 set -e
 
@@ -8,11 +8,11 @@ heroku git:remote --app cs501-t1-assessment --remote production
 git push production
 heroku run --remote production rails db:migrate
 
-# export FLASK_APP=project.server
-# export APP_SETTINGS="project.server.config.ProductionConfig"
-# export FLASK_ENV=production
+export FLASK_APP=project.server
+export APP_SETTINGS="project.server.config.ProductionConfig"
+export FLASK_ENV=production
 
-# flask db init
-# flask db migrate
-# flask db upgrade
-# flask run
+flask db init
+flask db migrate
+flask db upgrade
+flask run
