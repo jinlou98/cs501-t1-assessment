3aa3bdd9769a81f4c68134fe227f9bf5c981bb34
Jin Lou <jinlou@dhcp-wifi-8021x-155-41-86-6.bu.edu>
2021-09-13 13:17:18 -0400

    Edit migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,8 +1,18 @@
-export FLASK_APP=project.server
-export APP_SETTINGS="project.server.config.ProductionConfig"
-export FLASK_ENV=production
+#!/usr/bin/venv bash
 
-flask db init
-flask db migrate
-flask db upgrade
-flask run
+set -e
+
+echo "Deploying master to production"
+
+heroku git:remote --app cs501-t1-assessment --remote production
+git push production
+heroku run --remote production rails db:migrate
+
+# export FLASK_APP=project.server
+# export APP_SETTINGS="project.server.config.ProductionConfig"
+# export FLASK_ENV=production
+
+# flask db init
+# flask db migrate
+# flask db upgrade
+# flask run
